736f076d1bf35aefe28524408b55de51f54e6d5b
Muriithi Joel Nyaga <joelnyagamuriithi@gmail.com>
2022-09-16 18:46:21 +0300

    modifying create and update  in serializers.py

--- store/serializers.py
+++ store/serializers.py
@@ -42,6 +42,20 @@ class ProductSerializer(serializers.ModelSerializer):
         return product.unit_price * Decimal(1.1)
 
 
+'''     def create(self, validated_data):
+        product= Product(**validated_data)
+        # -------👇some other field
+        product.other=1
+        product.save()
+        return product
+    
+    def create(self, instance, validated_data):
+        # Instace is the product class
+        instance.unit_price = validated_data.get('unit_price')
+        instance.save()
+        return instance '''
+
+
 ''' 
     def validate(self, data):
         if data['password'] != data['confirm_password']:
