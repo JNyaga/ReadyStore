349122df1e01a5c5973e34609ae9be631eb11537
JNyaga <joelnyagamuriithi@gmail.com>
2023-03-18 16:50:45 +0300

    Before token based authentication

---
+++ core/serializers.py
@@ -0,0 +1,10 @@
+from djoser.serializers import UserCreateSerializer as BaseUserCreateSerializer
+from rest_framework import serializers
+
+
+class UserCreateSerializer(BaseUserCreateSerializer):
+    birth_date = serializers.DateField()
+
+    class Meta(BaseUserCreateSerializer.Meta):
+        fields = ['id', 'username', 'password',
+                  'email', 'first_name', 'last_name']
