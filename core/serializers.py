c139a0b927414667b48f1d13c80588adcfddc3e6
JNyaga <joelnyagamuriithi@gmail.com>
2023-03-18 20:57:41 +0300

    Creating the custom UserCreateSerializer with djoser

--- core/serializers.py
+++ core/serializers.py
@@ -3,8 +3,6 @@ from rest_framework import serializers
 
 
 class UserCreateSerializer(BaseUserCreateSerializer):
-    birth_date = serializers.DateField()
-
     class Meta(BaseUserCreateSerializer.Meta):
         fields = ['id', 'username', 'password',
                   'email', 'first_name', 'last_name']
