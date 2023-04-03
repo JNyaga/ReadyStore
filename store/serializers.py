94009d6e4525ced81425c7cff5fc162f90f17d99
JNyaga <joelnyagamuriithi@gmail.com>
2023-04-03 17:38:05 +0300

    changing from get or create to get

--- store/serializers.py
+++ store/serializers.py
@@ -195,7 +195,7 @@ class CreateOrderSerializer(serializers.Serializer):
         def save(self, **kwargs):
             cart_id = self.validated_data['cart_id']
 
-            (customer, created) = Customer.objects.get_or_create(
+            customer = Customer.objects.get(
                 user_id=self.context['user_id'])
             order = Order.objects.create(customer=customer)
 
